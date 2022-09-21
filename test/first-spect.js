fa2a663810b2790446d9bc9db4fdbf0efb823de5
lguaregua <guaregua19@gmail.com>
2022-09-21 13:07:08 -0400

    first commit

---
+++ test/first-spect.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+
+
+describe("A feature", () => {
+    describe("A scenario", () => {
+        it("Does something expected", () => {
+            assert(true);
+        })
+    })
+})
